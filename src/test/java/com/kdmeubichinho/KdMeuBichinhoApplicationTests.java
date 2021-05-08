bdede83dc9b9366ff1de05a12cedc90ba27cf11e
Mateus Araújo <mateusaraujo996@gmail.com>
2021-05-08 17:46:49 -0300

    Criação do Projeto KdMeuBichinho

---
+++ src/test/java/com/kdmeubichinho/KdMeuBichinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdmeubichinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdMeuBichinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
