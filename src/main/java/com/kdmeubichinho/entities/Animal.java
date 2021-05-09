97af9eb85380670b7398902d8ffe6ea9e34956ca
Mateus Araújo <mateusaraujo996@gmail.com>
2021-05-09 11:39:32 -0300

    Criação da entidade Animal

---
+++ src/main/java/com/kdmeubichinho/entities/Animal.java
@@ -0,0 +1,28 @@
+package com.kdmeubichinho.entities;
+
+public class Animal {
+	
+	private Integer idAnimal;
+	
+	private AnimalSexo sexo;
+	
+	private AnimalClassificacaoEtaria classificacaoEtaria;
+	
+	private AnimalPorte porte;
+	
+	private Boolean castrado;
+	
+	private Boolean vacinado;
+	
+	private String nome;
+	
+	private String cep;
+	private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String ddd;
+
+}
