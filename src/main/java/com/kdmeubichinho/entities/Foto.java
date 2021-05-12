1a0c6193f88b5dea71367ec6283d0fc31c1104dc
Mateus Araújo <mateusaraujo996@gmail.com>
2021-05-12 13:48:34 -0300

    Criação da entidade Foto

---
+++ src/main/java/com/kdmeubichinho/entities/Foto.java
@@ -0,0 +1,20 @@
+package com.kdmeubichinho.entities;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Foto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id_foto;
+	@Column(nullable = false)
+	private String caminho;
+//	@OneToOne
+//	@JoinColumn(name = "fk_id_animal")
+//	@JsonIgnoreProperties("fotos")
+//	private Animal idAnimal;
+	
+}
+}
