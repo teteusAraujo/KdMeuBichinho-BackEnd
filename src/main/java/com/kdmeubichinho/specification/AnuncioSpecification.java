5ac867fb21e4e6ab643ba37cd503df3716607e35
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-01 13:35:27 -0300

    Adcionando alguns filtros na especificação do anuncio

--- src/main/java/com/kdmeubichinho/specification/AnuncioSpecification.java
+++ src/main/java/com/kdmeubichinho/specification/AnuncioSpecification.java
@@ -18,28 +18,50 @@ public class AnuncioSpecification {
         return (root, criteriaQuery, criteriaBuilder) ->
                 criteriaBuilder.equal(root.get("status"), status);
     }
+	
     public static Specification<Anuncio> AnimalSexoFilter(AnimalSexo sexo) {
         return (root, criteriaQuery, criteriaBuilder) ->
                 criteriaBuilder.equal(root.join("idAnimal").get("sexo"), sexo);
     }
+    
     public static Specification<Anuncio> AnimalPorteFilter(AnimalPorte porte) {
         return (root, criteriaQuery, criteriaBuilder) ->
                 criteriaBuilder.equal(root.join("idAnimal").get("porte"), porte);
     }
+    
     public static Specification<Anuncio> AnimalClassificacaoEtariaFilter(AnimalClassificacaoEtaria classificacaoEtaria) {
         return (root, criteriaQuery, criteriaBuilder) ->
                 criteriaBuilder.equal(root.join("idAnimal").get("classificacaoEtaria"), classificacaoEtaria);
     }
+    
     public static Specification<Anuncio> AnimalCepFilter(String cep) {
         return (root, criteriaQuery, criteriaBuilder) ->
                 criteriaBuilder.like(root.join("idAnimal").get("cep"), contains(cep));
     }
+    
 	private static String contains(String expression) {
     return MessageFormat.format("{0}%", expression);
 	}
+	
     public static Specification<Anuncio> AnuncioCategoriaFilter(Integer idCategoria) {
         return (root, criteriaQuery, criteriaBuilder) ->
                 criteriaBuilder.equal(root.join("idCategoria").get("idCategoria"), idCategoria);
     }
+    
+    public static Specification<Anuncio> AnimalEspecieFilter(Integer idEspecie) {
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.join("idAnimal").get("especie"), idEspecie);
+    }
+    
+    public static Specification<Anuncio> AnimalVacinadoFilter(Boolean vacinado) {
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.join("idAnimal").get("vacinado"), vacinado);
+    }
+    
+    public static Specification<Anuncio> AnimalCastradoFilter(Boolean castrado) {
+        return (root, criteriaQuery, criteriaBuilder) ->
+                criteriaBuilder.equal(root.join("idAnimal").get("castrado"), castrado);
+    }
+
 
 }
