3ca698dbf93ac1b9159d1e178d6516480647aff7
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-02 20:07:13 -0300

    Criação da categoriaService

---
+++ src/main/java/com/kdmeubichinho/services/CategoriaService.java
@@ -0,0 +1,33 @@
+package com.kdmeubichinho.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kdmeubichinho.entities.Categoria;
+import com.kdmeubichinho.repositories.CategoriaRepository;
+
+public class CategoriaService {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	public Iterable<Categoria> getAllCategory() {
+		return categoriaRepository.findAll();
+	}
+
+	public Optional<Categoria> getCategoryById(Integer id) {
+		return categoriaRepository.findById(id);
+	}
+
+	public CategoriaRequestDTO addCategory(CategoriaRequestDTO categoryRequestDTO) {
+		categoriaRepository.save(categoryRequestDTO.build());
+		return categoryRequestDTO;
+	}
+
+	public void deleteCategory(Integer id) {
+		categoriaRepository.deleteById(id);
+	}
+
+
+}
