06e40f59e5576bf2bd9286a18aff7ed486f8d231
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-05 12:41:47 -0300

    Criação de Credencica e Pessoa DTO

---
+++ src/main/java/com/kdmeubichinho/dto/CredenciaisDTO.java
@@ -0,0 +1,11 @@
+package com.kdmeubichinho.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CredenciaisDTO {
+	private String email;
+	private String senha;
+}
---
+++ src/main/java/com/kdmeubichinho/dto/PessoaDTO.java
@@ -0,0 +1,32 @@
+package com.kdmeubichinho.dto;
+
+import com.kdmeubichinho.entities.Pessoa;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PessoaDTO {
+    private Integer idPessoa;
+    private String nome;
+    private String email;
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String ddd;
+    private String numeroResidencial;
+    private String celular;
+    private String senha;
+    
+    public Pessoa build() {
+    	return new Pessoa(this.idPessoa, this.nome, this.email, this.cep, this.logradouro, this.complemento, this.bairro, this.localidade,
+    			this.uf, this.ibge, this.ddd, this.numeroResidencial,	this.celular, this.senha);
+    }
+}
