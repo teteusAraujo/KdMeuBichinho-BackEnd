42f087447a16b93fc32b67931bdee74e43fdbb0c
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-07 20:19:37 -0300

    Criação do metodo addAnuncio e atualizarAnuncio

--- src/main/java/com/kdmeubichinho/controllers/AnuncioController.java
+++ src/main/java/com/kdmeubichinho/controllers/AnuncioController.java
@@ -5,11 +5,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kdmeubichinho.entities.Anuncio;
@@ -43,5 +48,16 @@ public class AnuncioController {
 	@GetMapping("/pessoa")
 	public Page<Anuncio> getAnnounceByEmailPessoa(@RequestParam String email, Pageable pageable) {
 		return anuncioService.getAnnounceByEmailPessoa(email, pageable);
+	}
+	
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public Anuncio addAnnounce(@RequestBody Anuncio anuncio) {
+		return anuncioService.addAnnounce(anuncio);
+	}
+
+	@PutMapping("atualizastatus/{idAnuncio}")
+	public Anuncio updateStatusAnnounce(@PathVariable Integer idAnuncio) throws Exception {
+		return anuncioService.updateStatusAnnounce(idAnuncio);
 	}
 }
