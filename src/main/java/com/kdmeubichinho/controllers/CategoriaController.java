d910ad2a9dfcfebeaf1e4a99054384ba2b1b6517
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-09 21:22:56 -0300

    Criação dos metodos de pegar um categoria por id e criar uma categoria

--- src/main/java/com/kdmeubichinho/controllers/CategoriaController.java
+++ src/main/java/com/kdmeubichinho/controllers/CategoriaController.java
@@ -1,11 +1,19 @@
 package com.kdmeubichinho.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.kdmeubichinho.dto.CategoriaRequestDTO;
 import com.kdmeubichinho.entities.Categoria;
 import com.kdmeubichinho.services.CategoriaService;
+
 
 @RestController
 @RequestMapping(path = "categoria")
@@ -17,6 +25,16 @@ public class CategoriaController {
 	@GetMapping
 	public Iterable<Categoria> getAllCategory() {
 		return categoriaService.getAllCategory();
+	}
+	
+	@GetMapping("/{id}")
+	public Optional<Categoria> getCategoryById(@PathVariable Integer id) {
+		return categoriaService.getCategoryById(id);
+	}
+
+	@PostMapping()
+	public CategoriaRequestDTO addCategory(@RequestBody CategoriaRequestDTO categoryRequestDTO) {
+		return categoriaService.addCategory(categoryRequestDTO);
 	}
 
 
