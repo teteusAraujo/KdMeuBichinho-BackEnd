cd8937d6b4cc0984fdc569fa056bf0a188458a2d
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-13 15:27:58 -0300

    Criação dos metodos de add e deletar uma especie

--- src/main/java/com/kdmeubichinho/controllers/EspecieController.java
+++ src/main/java/com/kdmeubichinho/controllers/EspecieController.java
@@ -3,13 +3,19 @@ package com.kdmeubichinho.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kdmeubichinho.dto.EspecieRequestDTO;
 import com.kdmeubichinho.entities.Especie;
 import com.kdmeubichinho.services.EspecieService;
+
+
 
 
 @RestController
@@ -27,6 +33,16 @@ public class EspecieController {
 	@GetMapping("/{id}")
 	public Optional<Especie> getSpeciesById(@PathVariable Integer id) {
 		return especieService.getSpeciesById(id);
+	}
+	
+	@PostMapping()
+	public EspecieRequestDTO addSpecies(@RequestBody EspecieRequestDTO speciesRequestDTO) {
+		return especieService.addSpecies(speciesRequestDTO);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteSpecies(@PathVariable Integer id) {
+		especieService.deleteSpecies(id);
 	}
 
 }
