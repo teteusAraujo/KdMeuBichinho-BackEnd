2949b7fc4918815c6117cb9f8fdfc6a50458c8b9
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-14 19:27:30 -0300

    Metodo de post de de enviar uma foto

--- src/main/java/com/kdmeubichinho/controllers/FotoController.java
+++ src/main/java/com/kdmeubichinho/controllers/FotoController.java
@@ -1,11 +1,19 @@
 package com.kdmeubichinho.controllers;
 
+import java.io.IOException;
+import java.util.Date;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.StringUtils;
+
 import com.kdmeubichinho.entities.Foto;
 import com.kdmeubichinho.repositories.FotoRepository;
 
@@ -24,5 +32,24 @@ public class FotoController {
 	@GetMapping("/{id}")
 	public Optional<Foto> getById(@PathVariable Integer id){
 		return fotoRepository.findById(id);
+	}
+	
+	@CrossOrigin
+	@PostMapping()
+	public String saveImg(@RequestParam("image") MultipartFile file) {
+		String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+		String uploadDir = "target/classes/static";
+		
+		Date date = new Date();
+		String filePrefix = date.getTime() + "-";
+		fileName = filePrefix + fileName;
+		
+		try {
+			FileUploadUtil.saveFile(uploadDir, fileName, file);			
+		} catch(IOException e) {
+			return ("Não foi possível salvar o arquivo: " + fileName);
+		}
+//		return uploadDir + "/" + fileName;
+		return fileName;
 	}
 }
