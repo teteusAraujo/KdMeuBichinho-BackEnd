3799e749ab023d82c3fd3ea01c861d29a7543d23
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-18 12:18:33 -0300

    Criação do metodo de atualizar e deletar uma foto

--- src/main/java/com/kdmeubichinho/controllers/FotoController.java
+++ src/main/java/com/kdmeubichinho/controllers/FotoController.java
@@ -5,9 +5,12 @@ import java.util.Date;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,7 +53,27 @@ public class FotoController {
 		} catch(IOException e) {
 			return ("Não foi possível salvar o arquivo: " + fileName);
 		}
-//		return uploadDir + "/" + fileName;
 		return fileName;
 	}
+	
+	@PutMapping("/{idFoto}")
+	public Foto updateFoto(@PathVariable Integer idFoto,@RequestBody Foto dadosFoto) throws Exception{
+		
+		
+		Foto meuFoto = fotoRepository.findById(idFoto)
+				.orElseThrow(()-> new IllegalAccessException());
+		
+
+		if(!dadosFoto.getCaminho().isEmpty()) meuFoto.setCaminho(dadosFoto.getCaminho());
+		
+		
+		fotoRepository.save(meuFoto);
+		return meuFoto;
+	}
+	
+		@DeleteMapping("/{id}")
+		public void deleteFoto(@PathVariable Integer id) {
+			fotoRepository.deleteById(id);
+		}
+
 }
