5895b76ba56b02db45f9a05247e32bb8f9d17656
Mateus Araújo <mateusaraujo996@gmail.com>
2021-06-22 22:04:13 -0300

    Implementando a classe PessoaService

---
+++ src/main/java/com/kdmeubichinho/services/PessoaService.java
@@ -0,0 +1,38 @@
+package com.kdmeubichinho.services;
+
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PessoaService implements UserDetailsService {
+	
+	@Autowired
+    private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+    private JwtService jwtService;
+    
+    @Autowired
+	private PasswordEncoder encoder;
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	public Iterable<Pessoa> getAllPersons(){
+		return pessoaRepository.findAll();
+	}
+	public Optional<Pessoa> getPersonById(Integer id){
+		return pessoaRepository.findById(id);
+	}
+	public Pessoa getPersonByEmail(String email) {
+		return pessoaRepository.findOneByEmail(email);
+	}
+	@Transactional
+    public Pessoa savePerson(PessoaDTO pessoa){
+        String senhaCriptografada = passwordEncoder.encode(pessoa.getSenha());
+        pessoa.setSenha(senhaCriptografada);
+        return pessoaRepository.save(pessoa.build());
+    }
+}
