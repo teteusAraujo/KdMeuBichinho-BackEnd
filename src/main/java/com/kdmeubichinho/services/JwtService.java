79bcb64a091431e35091366d9462fd3b0067d569
Mateus Araújo <mateusaraujo996@gmail.com>
2021-07-01 19:03:00 -0300

    Criar a função para gerar o Token

--- src/main/java/com/kdmeubichinho/services/JwtService.java
+++ src/main/java/com/kdmeubichinho/services/JwtService.java
@@ -1,6 +1,14 @@
 package com.kdmeubichinho.services;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
+
+import com.kdmeubichinho.entities.Pessoa;
+import io.jsonwebtoken.Jwts;
 
 public class JwtService {
 	
@@ -9,5 +17,19 @@ public class JwtService {
 	
 	@Value("${security.jwt.chave-assinatura}")
 	private String chaveAssinatura;
+	
+	public String gerarToken(Pessoa pessoa) {
+		long expString = Long.valueOf(expiracao);
+		LocalDateTime dataHoraExpiracao = LocalDateTime.now().plusDays(expString);
+		Instant instant = dataHoraExpiracao.atZone(ZoneId.systemDefault()).toInstant();
+		Date data = Date.from(instant);
+		
+		return Jwts
+				.builder()
+				.setSubject(pessoa.getEmail())
+				.setExpiration(data)
+				.signWith(SignatureAlgorithm.HS512, chaveAssinatura)
+				.compact();
+	}
 
 }
