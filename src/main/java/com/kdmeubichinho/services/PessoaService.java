65a08276594dfd40ed40206c23e4e8004916b331
Mateus Araújo <mateusaraujo996@gmail.com>
2021-07-14 10:26:00 -0300

    Criação do me metodo autenticar pessoa

--- src/main/java/com/kdmeubichinho/services/PessoaService.java
+++ src/main/java/com/kdmeubichinho/services/PessoaService.java
@@ -5,10 +5,17 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.server.ResponseStatusException;
 
+import com.kdmeubichinho.dto.CredenciaisDTO;
 import com.kdmeubichinho.dto.PessoaDTO;
+import com.kdmeubichinho.dto.TokenDTO;
 import com.kdmeubichinho.entities.Pessoa;
 import com.kdmeubichinho.repositories.PessoaRepository;
+
+
 
 public class PessoaService implements UserDetailsService {
 	
@@ -42,4 +49,20 @@ public class PessoaService implements UserDetailsService {
         pessoa.setSenha(senhaCriptografada);
         return pessoaRepository.save(pessoa.build());
     }
+	
+	public ResponseEntity<TokenDTO> authenticatePerson(CredenciaisDTO credenciais){
+        try{
+        	System.out.println(credenciais);
+            Pessoa pessoa = Pessoa.builder()
+                    .email(credenciais.getEmail())
+                    .senha(credenciais.getSenha()).build();
+            		authenticate(pessoa);
+            String token = jwtService.gerarToken(pessoa);
+            TokenDTO tokenDto =  new TokenDTO(pessoa.getEmail(), token);
+            return new ResponseEntity<TokenDTO>(tokenDto, HttpStatus.OK);
+            
+        } catch (UsernameNotFoundException | SenhaInvalidaException e ){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
+    }	
 }
