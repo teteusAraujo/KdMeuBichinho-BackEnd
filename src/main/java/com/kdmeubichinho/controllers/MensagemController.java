01efd3e1773f8b5498e1bb3c32b059434b4f35e5
Mateus Araújo <mateusaraujo996@gmail.com>
2021-08-11 23:30:04 -0300

    Adcionando o metodo de adcionar e atualizar uma mensagem

--- src/main/java/com/kdmeubichinho/controllers/MensagemController.java
+++ src/main/java/com/kdmeubichinho/controllers/MensagemController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,15 @@ public class MensagemController {
 	@GetMapping("/{id}")
 	public Optional<Mensagem> getMessageById(@PathVariable Integer id){
 		return mensagemService.getMessageById(id);
+	}
+	
+	@PostMapping()
+	public Mensagem addMessage(@RequestBody Mensagem message){
+		return mensagemService.addMessage(message);
+	}
+	
+	@PutMapping("/{idMessage}")
+	public Mensagem updateMessage(@PathVariable Integer idMessage,@RequestBody Mensagem dataMessage) throws Exception{
+		return mensagemService.updateMessage(idMessage, dataMessage);
 	}
 }
