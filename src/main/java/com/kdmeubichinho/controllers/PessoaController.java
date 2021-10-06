edca913d7584756e01daa9fdc72d4f4e1d0ffce4
Mateus Araújo <mateusaraujo996@gmail.com>
2021-10-06 20:38:32 -0300

    feat: Metodo post de salvar e autenticar uma pessoa

--- src/main/java/com/kdmeubichinho/controllers/PessoaController.java
+++ src/main/java/com/kdmeubichinho/controllers/PessoaController.java
@@ -3,15 +3,23 @@ package com.kdmeubichinho.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kdmeubichinho.dto.CredenciaisDTO;
+import com.kdmeubichinho.dto.TokenDTO;
 import com.kdmeubichinho.entities.Pessoa;
 import com.kdmeubichinho.services.PessoaService;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -37,6 +45,17 @@ public class PessoaController {
 	@GetMapping("/email")
 	public Optional<Pessoa> getPersonByEmail(@RequestParam String email) {
 		return pessoaService.getPersonByEmail(email);
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Pessoa salvar(@RequestBody com.kdmeubichinho.dto.PessoaDTO pessoa) {
+		return pessoaService.savePerson(pessoa);
+	}
+
+	@PostMapping("/auth")
+	public ResponseEntity<TokenDTO> autenticar(@RequestBody CredenciaisDTO credenciais) {
+		return pessoaService.authenticatePerson(credenciais);
 	}
 
 }
